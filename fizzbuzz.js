fe537f8eec928259127884a7e2843d1a7f0510e9
alexleon <alex@alexanderleon.com>
2014-12-08 12:26:08 -0500

    Added js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,1 @@
-A javascript fizz buzz one-liner. Took two tries to get it right. Forgot to log the output of the loop the first time.
-
 var i=1; for (; i<=100; i++) console.log( i % 3 === 0 ? (i % 15 === 0 ? 'fizzbuzz' : 'fizz') : (i % 5 == 0 ? 'buzz' : i) );
